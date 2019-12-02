ca3b3c935481bf0d3efe449ae7b39054f8765e2d
Germain <germain.clauss@receeve.com>
2019-12-02 10:29:02 +0100

    Fix: unit tests for typings

--- types/typings.test.ts
+++ types/typings.test.ts
@@ -5,5 +5,5 @@ type Holiday = Holidays.Holiday;
 const hd = new Holidays('');
 hd.init('US');
 
-hd.isHoliday(new Date('2019-06-04')); // $ExpectType Holiday
+hd.isHoliday(new Date('2019-06-04')); // $ExpectType Holiday | false
 hd.getHolidays(); // $ExpectType Holiday[]
