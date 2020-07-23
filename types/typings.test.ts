751b1492d5dec3b59d61e8043a817efd919d99b7
commenthol <commenthol@gmail.com>
2020-07-23 18:59:35 +0200

    break: isHolidays returns a list of Holiday objects

--- types/typings.test.ts
+++ types/typings.test.ts
@@ -1,9 +1,7 @@
 import * as Holidays from 'date-holidays';
 
-type Holiday = Holidays.Holiday;
-
 const hd = new Holidays('');
 hd.init('US');
 
-hd.isHoliday(new Date('2019-06-04')); // $ExpectType false | Holiday
+hd.isHoliday(new Date('2019-06-04')); // $ExpectType false | Holiday[]
 hd.getHolidays(); // $ExpectType Holiday[]
