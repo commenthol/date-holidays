11f690d2f9afa478138ebe7892b49503daadb417
commenthol <commenthol@gmail.com>
2021-03-27 15:31:21 +0100

    chore(gh-pages): update

--- index.js
+++ index.js
@@ -149,36 +149,55 @@ function vcalendar(dates, opts) {
 
 module.exports = vcalendar;
 },{"./templates":1}],3:[function(require,module,exports){
-/* global Holidays */
-
 ;(function () {
-  var vcalendar = require('date-holidays-ical/lib/vcalendar')
+  const vcalendar = require('date-holidays-ical/lib/vcalendar')
 
-  var DAY = 86400000
-  var HOUR = 3600000
+  const DAY = 86400000
+  const HOUR = 3600000
 
-  var WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  var year = new Date().getFullYear()
-  var g = { year: year }
-  var n = {}
+  const Holidays = window.Holidays.default
+  const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  const thisYear = () => new Date().getFullYear()
+  const g = { year: thisYear(), country: 'CA', language: 'fr' }
+  const n = {}
+  const selects = {
+    year: select('year'),
+    country: select('country', selectState),
+    state: select('state', selectRegion),
+    region: select('region'),
+    language: select('language')
+  }
+
+  function escapeHtml (string) {
+    return (string || '')
+      .replace(/&amp;/g, '&')
+      .replace(/[&<>'"]/g, tag => ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&#39;',
+        '"': '&quot;'
+      }[tag]))
+  }
 
   function duration (ms) {
-    var days = (ms / DAY) | 0
-    var hours = (ms - (days * DAY)) / HOUR | 0
-    var str = days ? days + 'd ' : ''
+    const days = (ms / DAY) | 0
+    const hours = (ms - (days * DAY)) / HOUR | 0
+    let str = days ? days + 'd ' : ''
     str += hours ? hours + 'h' : ''
     return str
   }
 
   function select (id, fn) {
-    var el = document.getElementById(id)
-    var self = {
+    const el = document.getElementById(id)
+    const self = {
       opts: {},
       onChange: function (ev) {
         self.selected = ev.target.value
         g[id] = self.selected
         n[id] = ev.target.selectedOptions && ev.target.selectedOptions[0].text
         if (id === 'country') {
+          g.language = undefined
           ;['state', 'region'].forEach(function (i) {
             n[i] = g[i] = undefined
             select(i).disable()
@@ -189,7 +208,7 @@ module.exports = vcalendar;
             select(i).disable()
           })
         }
-        renderContent()
+        route()
         fn && fn()
       },
       disable: function () {
@@ -218,43 +237,56 @@ module.exports = vcalendar;
   }
 
   function selectYear () {
-    var obj = {}
-    for (var i = year; i < year + 10; i++) {
+    const obj = {}
+    const year = g.year
+    for (let i = year - 5; i <= year + 5; i++) {
       obj[i] = i
     }
-    select('year').render(obj, year)
+    selects.year.render(obj, year)
   }
-  function selectCountry (code, name) {
-    var hd = new Holidays()
-    var cs = hd.getCountries()
-    var s = select('country', selectState)
-    s.render(cs, code)
-    if (code) s.onChange({ target: { value: code, selectedOptions: [ { text: name } ] } })
+  function selectCountry () {
+    const hd = new Holidays()
+    const cs = hd.getCountries(g.language)
+    g.country = cs[g.country] ? g.country : 'CA'
+    n.country = cs[g.country]
+    selects.country.render(cs, g.country)
   }
   function selectState () {
-    var hd = new Holidays()
-    var cs = hd.getStates(g.country)
-    select('state', selectRegion).render(cs)
+    const hd = new Holidays()
+    const cs = hd.getStates(g.country)
+    g.state = cs && cs[g.state] ? g.state : ''
+    n.state = cs && cs[g.state]
+    selects.state.render(cs, g.state)
   }
   function selectRegion () {
-    var hd = new Holidays()
-    var cs = hd.getRegions(g.country, g.state)
-    select('region').render(cs)
+    const hd = new Holidays()
+    const cs = hd.getRegions(g.country, g.state)
+    g.region = cs && cs[g.region] ? g.region : ''
+    n.state = cs && cs[g.region]
+    selects.region.render(cs, g.region)
+  }
+  function selectLanguage () {
+    const hd = new Holidays()
+    hd.init(g.country)
+    const cs = hd.getLanguages()
+    g.language = cs.includes(g.language) ? g.language : cs[0]
+    const cso = cs.reduce((o, k) => { o[k] = k; return o }, {})
+    selects.language.render(cso, g.language)
   }
 
   function renderContent () {
-    var hd = new Holidays(g.country, g.state, g.region)
-    var holidays = g.holidays = hd.getHolidays(g.year)
-    var count = 0
-    var table = [
+    const hd = new Holidays(g.country, g.state, g.region, { languages: g.language })
+    const holidays = g.holidays = hd.getHolidays(g.year)
+    let count = 0
+    const table = [
       '<table>',
       '<thead><tr><th>',
       ['#', 'weekday', 'date', 'duration', 'name', 'type'].join('</th><th>'),
       '</th></tr></thead>',
       '<tbody>',
       Object.keys(holidays).map(function (i) {
-        var d = holidays[i]
-        var _date = d.date.replace(/^(\d+-\d+-\d+ \d+:\d+:\d+).*$/, '$1')
+        const d = holidays[i]
+        const _date = d.date.replace(/^(\d+-\d+-\d+ \d+:\d+:\d+).*$/, '$1')
         return '<tr><td>' + [
           count++,
           WEEKDAYS[new Date(_date).getDay()],
@@ -276,8 +308,8 @@ module.exports = vcalendar;
   }
 
   function download (filename, dates) {
-    var fullday = document.getElementById('fullday').checked
-    var el = document.createElement('a')
+    const fullday = document.getElementById('fullday').checked
+    const el = document.createElement('a')
     el.setAttribute('href', 'data:text/calendar;charset=utf-8,' +
       encodeURIComponent(vcalendar(dates, { fullday: fullday })))
     el.setAttribute('download', filename)
@@ -288,13 +320,34 @@ module.exports = vcalendar;
     document.body.removeChild(el)
   }
   function onDownload () {
-    var filename = [g.year, n.country, n.state, n.region]
-      .filter(function (i) { return i }).join('-') + '.ics'
+    const filename = [g.year, n.country, n.state, n.region, g.language]
+      .filter(Boolean).join('-') + '.ics'
     download(filename, g.holidays)
   }
 
-  selectYear()
-  selectCountry('CA', 'Canada')
+  function route () {
+    const { language = '-', year, country, state = '', region = '' } = g
+    window.location.hash = '#' + [language, year, country, state, region].filter(Boolean).join('/')
+  }
+
+  function router () {
+    const { hash } = window.location
+    if (hash && hash.length > 1) {
+      let [language, year, country, state, region] = hash.substring(1).split('/').map(escapeHtml)
+      if (isNaN(Number(year))) year = thisYear()
+      if (language === '-') language = undefined
+      Object.assign(g, { language, year: Number(year), country, state, region })
+    }
+    selectYear()
+    selectCountry()
+    selectLanguage()
+    selectState()
+    selectRegion()
+    renderContent()
+  }
+
+  window.addEventListener('hashchange', router, false)
+  router()
 }())
 
 },{"date-holidays-ical/lib/vcalendar":2}]},{},[3]);
