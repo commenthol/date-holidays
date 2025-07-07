2aef53c0ed1cff600aeaff380b7de1fee38f2250
mrazauskas <tom@mrazauskas.de>
2025-07-07 08:27:07 +0300

    clean up

--- types/index.tst.ts
+++ types/index.tst.ts
@@ -5,6 +5,6 @@ const hd = new Holidays();
 
 expect(hd.init('US')).type.toBe<void>();
 
-expect(hd.isHoliday(new Date('2019-01-01'))).type.toBe<false | HolidaysTypes.Holiday[]>();
-expect(hd.isHoliday(new Date('2019-02-01'))).type.toBe<false | HolidaysTypes.Holiday[]>();
+expect(hd.isHoliday(new Date('2019-01-01'))).type.toBe<HolidaysTypes.Holiday[] | false>();
+expect(hd.isHoliday(new Date('2019-02-01'))).type.toBe<HolidaysTypes.Holiday[] | false>();
 expect(hd.getHolidays()).type.toBe<HolidaysTypes.Holiday[]>();
